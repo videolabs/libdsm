6f501eb3b02e559b90575d5ea53272a7dc57555c
Julien 'Lta' BALLET <contact@lta.io>
2014-04-30 16:57:11 +0200

    Adds Heimdal GSSAPI to build system, adds extended security to session flag

--- src/smb_message.c
+++ src/smb_message.c
@@ -185,8 +185,8 @@ void            smb_message_set_default_flags(smb_message *msg)
   assert(msg != NULL && msg->packet != NULL);
 
   msg->packet->header.flags   = 0x18;
-  //msg->packet->header.flags2  = 0xc843; // w/ extended security;
-  msg->packet->header.flags2  = 0xc043; // w/o extended security;
+  msg->packet->header.flags2  = 0xc843; // w/ extended security;
+  //msg->packet->header.flags2  = 0xc043; // w/o extended security;
 }
 
 void            smb_message_set_andx_members(smb_message *msg)
