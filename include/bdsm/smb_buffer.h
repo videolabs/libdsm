955690d3834772b95e026b30d476ccea0adf4454
Julien 'Lta' BALLET <contact@lta.io>
2014-05-28 15:49:29 +0200

    Add smb_buffer structure with related management functions

---
+++ include/bdsm/smb_buffer.h
@@ -0,0 +1,64 @@
+//---------------------------------------------------------------------------
+//  __________________    _________  _____            _____  .__         ._.
+//  \______   \______ \  /   _____/ /     \          /  _  \ |__| ____   | |
+//   |    |  _/|    |  \ \_____  \ /  \ /  \        /  /_\  \|  _/ __ \  | |
+//   |    |   \|    `   \/        /    Y    \      /    |    |  \  ___/   \|
+//   |______  /_______  /_______  \____|__  / /\   \____|__  |__|\___ |   __
+//          \/        \/        \/        \/  )/           \/        \/   \/
+//
+// This file is part of libdsm. Copyright © 2014 VideoLabs SAS
+//
+// Author: Julien 'Lta' BALLET <contact@lta.io>
+//
+// This program is free software. It comes without any warranty, to the extent
+// permitted by applicable law. You can redistribute it and/or modify it under
+// the terms of the Do What The Fuck You Want To Public License, Version 2, as
+// published by Sam Hocevar. See the COPYING file for more details.
+//----------------------------------------------------------------------------
+
+/**
+ * @file smb_types.h
+ * @brief liBDSM types and structures
+ */
+
+#ifndef __BDSM_SMB_BUFFER_H_
+#define __BDSM_SMB_BUFFER_H_
+
+#include <stdlib.h>
+
+/**
+  * @struct smb_buffer
+  * @brief Hold a pointer and the size of its data
+  */
+typedef struct
+{
+  void      *data;  /// Data pointed
+  size_t    size;   /// Size in byte of the pointed
+} smb_buffer;
+
+/**
+ * @brief Initialize a buffer structure. It'll contain nothing
+ *
+ * @param buf Pointer to a buffer to initialize
+ */
+void    smb_buffer_init(smb_buffer *buf, void *data, size_t size);
+
+/**
+ * @brief Allocate a size long memory area and place it in the buffer structure
+ */
+void    smb_buffer_alloc(smb_buffer *buf, size_t size);
+
+/**
+ * @brief Allocate a size long memory area from the stack and place it in
+ *  the buffer structure
+ */
+void    smb_buffer_alloca(smb_buffer *buf, size_t size);
+
+/**
+ * @brief Free the data of this buffer if necessary
+ *
+ * @param buf Pointer to a buffer to free
+ */
+void    smb_buffer_free(smb_buffer *buf);
+
+#endif
---
+++ src/smb_buffer.c
@@ -0,0 +1,60 @@
+//---------------------------------------------------------------------------
+//  __________________    _________  _____            _____  .__         ._.
+//  \______   \______ \  /   _____/ /     \          /  _  \ |__| ____   | |
+//   |    |  _/|    |  \ \_____  \ /  \ /  \        /  /_\  \|  _/ __ \  | |
+//   |    |   \|    `   \/        /    Y    \      /    |    |  \  ___/   \|
+//   |______  /_______  /_______  \____|__  / /\   \____|__  |__|\___ |   __
+//          \/        \/        \/        \/  )/           \/        \/   \/
+//
+// This file is part of libdsm. Copyright © 2014 VideoLabs SAS
+//
+// Author: Julien 'Lta' BALLET <contact@lta.io>
+//
+// This program is free software. It comes without any warranty, to the extent
+// permitted by applicable law. You can redistribute it and/or modify it under
+// the terms of the Do What The Fuck You Want To Public License, Version 2, as
+// published by Sam Hocevar. See the COPYING file for more details.
+//----------------------------------------------------------------------------
+
+#include <alloca.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#include "bdsm/smb_buffer.h"
+
+void    smb_buffer_init(smb_buffer *buf, void *data, size_t size)
+{
+  assert(buf != NULL);
+
+  buf->data = data;
+  buf->size = size;
+}
+
+void    smb_buffer_alloc(smb_buffer *buf, size_t size)
+{
+  assert(buf != NULL);
+
+  buf->data = malloc(size);
+  buf->size = size;
+
+  assert(buf->data != NULL);
+}
+
+void    smb_buffer_alloca(smb_buffer *buf, size_t size)
+{
+  assert(buf != NULL);
+
+  buf->data = alloca(size);
+  buf->size = size;
+
+  assert(buf->data != NULL);
+}
+
+void    smb_buffer_free(smb_buffer *buf)
+{
+  if (buf == NULL || buf->data == NULL)
+    return;
+
+  free(buf->data);
+  smb_buffer_init(buf, NULL, 0);
+}
