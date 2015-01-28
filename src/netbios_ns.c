60c4bea9d649f65bc200bd972fca13f559932a60
Thomas Guillem <thomas@gllm.fr>
2015-01-28 11:16:37 +0100

    netbios_ns: improve netbios_ns_recv
    - It use the same buffer for all recv.
    - It can now wait for a reply from a specific ip.
    - It can check if trn_id correspond with the last sent.
    - It can return a struct containing the parsed name_query. Parsing do more size
      check and type check.

--- src/netbios_ns.c
+++ src/netbios_ns.c
@@ -54,13 +54,32 @@ struct netbios_ns_entry
     time_t                        last_time_seen;
 };
 
+#define RECV_BUFFER_SIZE 1500 // Max MTU frame size for ethernet
+
 struct netbios_ns
 {
     int                 socket;
     struct sockaddr_in  addr;
     uint16_t            last_trn_id;  // Last transaction id used;
     netbios_ns_entry    *entries;     // NS entries cache, mainly used by discover()
+    uint8_t             buffer[RECV_BUFFER_SIZE];
     int                 abort_pipe[2];
+};
+
+typedef struct netbios_ns_name_query netbios_ns_name_query;
+struct netbios_ns_name_query
+{
+    enum name_query_type type;
+    union {
+        struct {
+            uint32_t ip;
+        } nb;
+        struct {
+            const char *name;
+            const char *group;
+            char type;
+        } nbstat;
+    }u;
 };
 
 static netbios_ns_entry *netbios_ns_inverse_internal(netbios_ns *ns,
@@ -122,6 +141,10 @@ static void   ns_close_abort_pipe(netbios_ns *ns)
     }
 }
 
+static uint16_t query_type_nb = 0x2000;
+static uint16_t query_type_nbstat = 0x2100;
+static uint16_t query_class_in = 0x0100;
+
 static int netbios_ns_send_name_query(netbios_ns *ns,
                                       uint32_t ip,
                                       enum name_query_type type,
@@ -131,9 +154,7 @@ static int netbios_ns_send_name_query(netbios_ns *ns,
     struct sockaddr_in  addr;
     ssize_t             sent;
     uint16_t            trn_id;
-    const char          *query_type;
-    static char         query_type_nb[4] = { 0x00, 0x20, 0x00, 0x01 };
-    static char         query_type_nbstat[4] = { 0x00, 0x21, 0x00, 0x01 };
+    uint16_t            query_type;
     netbios_query       *q;
 
     assert(ns != NULL);
@@ -160,7 +181,8 @@ static int netbios_ns_send_name_query(netbios_ns *ns,
     netbios_query_append(q, name, strlen(name) + 1);
 
     // Magic footer (i.e. Question type (Netbios) / class (IP)
-    netbios_query_append(q, query_type, 4);
+    netbios_query_append(q, (const char *)&query_type, 2);
+    netbios_query_append(q, (const char *)&query_class_in, 2);
     q->packet->queries = htons(1);
 
     if (ip == 0)
@@ -189,6 +211,204 @@ static int netbios_ns_send_name_query(netbios_ns *ns,
 
     ns->last_trn_id = trn_id; // Remember the last transaction id.
     return 0;
+}
+
+static int netbios_ns_handle_query(netbios_ns *ns, size_t size,
+                                   bool check_trn_id, uint32_t recv_ip,
+                                   netbios_ns_name_query *out_name_query)
+{
+    netbios_query_packet *q;
+    uint8_t name_size;
+    uint16_t *p_type, type;
+    uint16_t *p_data_length, data_length;
+    char     *p_data;
+
+    // check for packet size
+    if (size < sizeof(netbios_query_packet))
+    {
+        BDSM_dbg("netbios_ns_handle_query, invalid size !\n");
+        return -1;
+    }
+
+    q = (netbios_query_packet *)ns->buffer;
+    if (check_trn_id)
+    {
+        // check if trn_id corresponds
+        if (ntohs(q->trn_id) != ns->last_trn_id) {
+            BDSM_dbg("netbios_ns_handle_query, invalid trn_id: %d vs %d\n",
+                     ntohs(q->trn_id), ns->last_trn_id);
+            return -1;
+        }
+    }
+
+    if (!out_name_query)
+        return 0;
+
+    // get Name size, should be 0x20
+    if (size < sizeof(netbios_query_packet) + 1)
+        return -1;
+    name_size = q->payload[0];
+    if (name_size != 0x20)
+        return -1;
+
+    // get type and data_length
+    if (size < sizeof(netbios_query_packet) + name_size + 11)
+        return -1;
+    p_type = (uint16_t *) (q->payload + name_size + 2);
+    type = *p_type;
+    p_data_length = (uint16_t *) (q->payload + name_size + 10);
+    data_length = ntohs(*p_data_length);
+
+    if (size < sizeof(netbios_query_packet) + name_size + 12 + data_length)
+        return -1;
+    p_data = q->payload + name_size + 12;
+
+    if (type == query_type_nb) {
+        uint32_t ip;
+
+        // get ip addr
+        if (data_length < 6)
+            ip = recv_ip;
+        else
+        {
+            uint32_t *p_ip = (uint32_t *) (p_data + 2);
+            ip = *p_ip;
+        }
+
+        out_name_query->type = NAME_QUERY_TYPE_NB;
+        out_name_query->u.nb.ip = ip;
+    } else if (type == query_type_nbstat) {
+        uint8_t name_count;
+        const char *names = p_data +  1;
+        const char *group = NULL, *name = NULL;;
+
+        // get the number of names
+        if (data_length < 1)
+            return -1;
+        name_count = *(p_data);
+        names = p_data + 1;
+
+        if (data_length < name_count * 18)
+            return -1;
+
+        // first search for a group in the name list
+        for (uint8_t name_idx = 0; name_idx < name_count; name_idx++)
+        {
+            const char *current_name = names + name_idx * 18;
+            uint16_t current_flags = (current_name[16] << 8) | current_name[17];
+            if (current_flags & NETBIOS_NAME_FLAG_GROUP) {
+                group = current_name;
+                break;
+            }
+        }
+        // then search for file servers
+        for (uint8_t name_idx = 0; name_idx < name_count; name_idx++)
+        {
+            const char *current_name = names + name_idx * 18;
+            char current_type = current_name[15];
+            uint16_t current_flags = (current_name[16] << 8) | current_name[17];
+
+            if (current_flags & NETBIOS_NAME_FLAG_GROUP)
+                continue;
+            if (current_type == NETBIOS_FILESERVER)
+            {
+                name = current_name;
+                BDSM_dbg("netbios_ns_handle_query, Found name: '%.*s' in group: '%.*s'\n",
+                         NETBIOS_NAME_LENGTH, name, NETBIOS_NAME_LENGTH, group);
+                break;
+            }
+        }
+
+        if (name)
+        {
+            out_name_query->type = NAME_QUERY_TYPE_NBSTAT;
+            out_name_query->u.nbstat.name = name;
+            out_name_query->u.nbstat.group = group;
+            out_name_query->u.nbstat.type = NETBIOS_FILESERVER;
+        }
+    }
+
+    return 0;
+}
+
+static ssize_t netbios_ns_recv(netbios_ns *ns,
+                               struct timeval *timeout,
+                               struct sockaddr_in *out_addr,
+                               bool check_trn_id,
+                               uint32_t wait_ip,
+                               netbios_ns_name_query *out_name_query)
+{
+    int sock, abort_fd;
+
+    assert(ns != NULL);
+
+    sock = ns->socket;
+    abort_fd =  ns->abort_pipe[0];
+
+    if (out_name_query)
+        out_name_query->type = NAME_QUERY_TYPE_INVALID;
+
+    while (true)
+    {
+        fd_set read_fds, error_fds;
+        int res, nfds;
+
+        FD_ZERO(&read_fds);
+        FD_ZERO(&error_fds);
+        FD_SET(sock, &read_fds);
+        FD_SET(abort_fd, &read_fds);
+        FD_SET(sock, &error_fds);
+        nfds = (sock > abort_fd ? sock : abort_fd) + 1;
+
+        res = select(nfds, &read_fds, 0, &error_fds, timeout);
+
+        if (res < 0)
+            goto error;
+        if (FD_ISSET(sock, &error_fds))
+            goto error;
+
+        if (FD_ISSET(abort_fd, &read_fds))
+            return -1;
+
+        else if (FD_ISSET(sock, &read_fds))
+        {
+            struct sockaddr_in addr;
+            socklen_t addr_len = sizeof(struct sockaddr_in);
+            ssize_t size;
+
+            size = recvfrom(sock, ns->buffer, RECV_BUFFER_SIZE, 0,
+                           (struct sockaddr *)&addr, &addr_len);
+            if (size < 0)
+                return -1;
+            if (wait_ip != 0 && addr_len >= sizeof(struct sockaddr_in))
+            {
+                // wait for a reply from a specific ip
+                if (wait_ip != addr.sin_addr.s_addr)
+                {
+                    BDSM_dbg("netbios_ns_recv, invalid ip");
+                    continue;
+                }
+            }
+
+            if (netbios_ns_handle_query(ns, (size_t)size, check_trn_id,
+                                        addr.sin_addr.s_addr,
+                                        out_name_query) == -1)
+            {
+                BDSM_dbg("netbios_ns_recv, invalid query\n");
+                continue;
+            }
+
+            if (out_addr)
+                *out_addr = addr;
+            return size;
+        }
+        else
+            return (0);
+    }
+
+error:
+    BDSM_perror("netbios_ns_recv: ");
+    return (-1);
 }
 
 static void netbios_ns_copy_name(char *dest, const char *src)
@@ -289,57 +509,15 @@ void          netbios_ns_destroy(netbios_ns *ns)
     free(ns);
 }
 
-static ssize_t    netbios_ns_recv(int sock, int abort_fd,
-                                  void *buf, size_t buf_size,
-                                  struct timeval *timeout, struct sockaddr *addr,
-                                  socklen_t *addr_len)
-{
-    fd_set        read_fds, error_fds;
-    int           res, nfds;
-
-    assert(sock && buf != NULL && buf_size > 0);
-
-    FD_ZERO(&read_fds);
-    FD_ZERO(&error_fds);
-    FD_SET(sock, &read_fds);
-    FD_SET(abort_fd, &read_fds);
-    FD_SET(sock, &error_fds);
-    nfds = (sock > abort_fd ? sock : abort_fd) + 1;
-
-    res = select(nfds, &read_fds, 0, &error_fds, timeout);
-
-    if (res < 0)
-        goto error;
-    if (FD_ISSET(sock, &error_fds))
-        goto error;
-
-    if (FD_ISSET(abort_fd, &read_fds))
-    {
-        return (-1);
-    }
-    else if (FD_ISSET(sock, &read_fds))
-    {
-        return (recvfrom(sock, buf, buf_size, 0, addr, addr_len));
-    }
-    else
-        return (0);
-
-error:
-    BDSM_perror("netbios_ns_recv: ");
-    return (-1);
-}
-
 int      netbios_ns_resolve(netbios_ns *ns, const char *name, char type, uint32_t *addr)
 {
     netbios_ns_entry    *cached;
     struct timeval      timeout;
     char                *encoded_name;
-    char                recv_buffer[512]; // Hu ?
     ssize_t             recv;
-
+    netbios_ns_name_query name_query;
 
     assert(ns != NULL);
-
 
     if ((cached = netbios_ns_entry_find(ns, name, 0)) != NULL)
     {
@@ -362,18 +540,19 @@ int      netbios_ns_resolve(netbios_ns *ns, const char *name, char type, uint32_
     // Now wait for a reply and pray
     timeout.tv_sec = 2;
     timeout.tv_usec = 420;
-    recv = netbios_ns_recv(ns->socket, ns->abort_pipe[0],
-                           (void *)recv_buffer, 512, &timeout, 0, 0);
+    recv = netbios_ns_recv(ns, &timeout, NULL, true, 0, &name_query);
 
     if (recv < 0)
         BDSM_perror("netbios_ns_resolve:");
-    else if (recv == 0)
-        BDSM_dbg("netbios_ns_resolve, received NO reply for '%s' !\n", name);
     else
     {
-        BDSM_dbg("netbios_ns_resolve, received a reply for '%s' !\n", name);
-        *addr = (*(uint32_t *)(recv_buffer + recv - 4));
-        return (1);
+        if (name_query.type == NAME_QUERY_TYPE_NB)
+        {
+            *addr = name_query.u.nb.ip;
+            BDSM_dbg("netbios_ns_resolve, received a reply for '%s', ip: 0x%X!\n", name, *addr);
+            return (1);
+        } else
+            BDSM_dbg("netbios_ns_resolve, wrong query type received\n");
     }
 
     return (0);
@@ -381,20 +560,15 @@ int      netbios_ns_resolve(netbios_ns *ns, const char *name, char type, uint32_
 
 // We have a small recursive function for discovery, to stack received reply
 // when descending, and performing reverse lookup when ascending
-static void   netbios_ns_discover_rec(netbios_ns *ns, struct timeval *timeout,
-                                      void *recv_buffer)
+static void netbios_ns_discover_rec(netbios_ns *ns, struct timeval *timeout )
 {
     struct sockaddr_in  recv_addr;
-    socklen_t           recv_addr_len;
     int                 res;
 
-    recv_addr_len = sizeof(recv_addr);
-    res = netbios_ns_recv(ns->socket, ns->abort_pipe[0],
-                          recv_buffer, 256, timeout,
-                          (struct sockaddr *)&recv_addr, &recv_addr_len);
+    res = netbios_ns_recv(ns, timeout, &recv_addr, true, 0, NULL);
     if (res > 0 && timeout->tv_sec && timeout->tv_usec)
     {
-        netbios_ns_discover_rec(ns, timeout, recv_buffer);
+        netbios_ns_discover_rec(ns, timeout);
 
         BDSM_dbg("Discover: received a reply from %s\n",
                  inet_ntoa(recv_addr.sin_addr));
@@ -405,7 +579,6 @@ static void   netbios_ns_discover_rec(netbios_ns *ns, struct timeval *timeout,
 int           netbios_ns_discover(netbios_ns *ns)
 {
     struct timeval      timeout;
-    char                recv_buffer[256];
 
     assert(ns != NULL);
 
@@ -422,7 +595,7 @@ int           netbios_ns_discover(netbios_ns *ns)
     //
     timeout.tv_sec = 2;
     timeout.tv_usec = 420;
-    netbios_ns_discover_rec(ns, &timeout, (void *)recv_buffer);
+    netbios_ns_discover_rec(ns, &timeout);
 
     return (1);
 }
@@ -439,8 +612,8 @@ static netbios_ns_entry *netbios_ns_inverse_internal(netbios_ns *ns, uint32_t ip
 {
     netbios_ns_entry  *cached;
     struct timeval      timeout;
-    char                recv_buffer[512]; // Hu ?
     ssize_t             recv;
+    netbios_ns_name_query name_query;
 
     if ((cached = netbios_ns_entry_find(ns, NULL, ip)) != NULL)
         return (cached);
@@ -452,56 +625,23 @@ static netbios_ns_entry *netbios_ns_inverse_internal(netbios_ns *ns, uint32_t ip
     // Now wait for a reply and pray
     timeout.tv_sec = 1;
     timeout.tv_usec = 500;
-    recv = netbios_ns_recv(ns->socket, ns->abort_pipe[0],
-                           (void *)recv_buffer, 512, &timeout, 0, 0);
+    recv = netbios_ns_recv(ns, &timeout, NULL, true, ip, &name_query);
 
     if (recv <= 0)
         goto error;
+    
+    if (name_query.type != NAME_QUERY_TYPE_NBSTAT)
+    {
+        BDSM_dbg("netbios_ns_inverse, wrong query type received\n");
+        goto error;
+    }
     else
         BDSM_dbg("netbios_ns_inverse, received a reply for '%s' !\n",
                  inet_ntoa(*(struct in_addr *)&ip));
 
-
-    // Now we've got something, let's find the <20>/<0> name
-    netbios_query_packet  *p = (netbios_query_packet *)recv_buffer;
-    uint8_t                 name_count;
-    uint8_t                 name_idx;
-    const char              *names;
-    const char              *group = NULL;
-    netbios_ns_entry        *entry = NULL;
-
-    BDSM_dbg("Queried name length: %u\n", p->payload[0]);
-    name_count = p->payload[p->payload[0] + 12];
-    BDSM_dbg("Number of names: %hhu\n", name_count);
-    names = p->payload + p->payload[0] + 13;
-
-    // first search for a group in the name list
-    for (name_idx = 0; name_idx < name_count; name_idx++)
-    {
-        const char *current_name = names + name_idx * 18;
-        uint16_t current_flags = (current_name[16] << 8) | current_name[17];
-        if (current_flags & NETBIOS_NAME_FLAG_GROUP) {
-            group = current_name;
-            BDSM_dbg("Found group: %s\n", group);
-            break;
-        }
-    }
-    // then search for file servers
-    for (name_idx = 0; name_idx < name_count; name_idx++)
-    {
-        const char *current_name = names + name_idx * 18;
-        char current_type = current_name[15];
-        uint16_t current_flags = (current_name[16] << 8) | current_name[17];
-
-        if (current_flags & NETBIOS_NAME_FLAG_GROUP)
-            continue;
-        BDSM_dbg("Found name : %s (type == 0x%x)\n", current_name, current_type);
-        if (current_type == NETBIOS_FILESERVER)
-            entry = netbios_ns_entry_add(ns, current_name, group, current_type, ip);
-    }
-
-    return (entry);
-
+    return netbios_ns_entry_add(ns, name_query.u.nbstat.name,
+                                name_query.u.nbstat.group,
+                                name_query.u.nbstat.type, ip);
 error:
     BDSM_perror("netbios_ns_inverse: ");
     return (NULL);
