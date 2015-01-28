8ca63e5036b838454a29e5cb4a45cdec6d263627
Thomas Guillem <thomas@gllm.fr>
2015-01-28 16:50:24 +0100

    netbios_ns: merge with netbios_ns_entry

--- src/netbios_ns.c
+++ src/netbios_ns.c
@@ -19,6 +19,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include <unistd.h>
 #include <fcntl.h>
@@ -30,8 +31,9 @@
 #include <arpa/inet.h>
 #include <sys/select.h>
 
+#include <bdsm/netbios_ns.h>
+
 #include "bdsm_debug.h"
-#include "netbios_ns.h"
 #include "netbios_query.h"
 #include "netbios_utils.h"
 
@@ -41,6 +43,25 @@ enum name_query_type {
     NAME_QUERY_TYPE_NBSTAT
 };
 static char name_query_broadcast[] = NETBIOS_WILDCARD;
+
+struct netbios_ns_entry
+{
+    netbios_ns_entry              *next;
+    struct in_addr                address;
+    char                          name[NETBIOS_NAME_LENGTH + 1];
+    char                          group[NETBIOS_NAME_LENGTH + 1];
+    char                          type;
+    time_t                        last_time_seen;
+};
+
+struct netbios_ns
+{
+    int                 socket;
+    struct sockaddr_in  addr;
+    uint16_t            last_trn_id;  // Last transaction id used;
+    netbios_ns_entry    *entries;     // NS entries cache, mainly used by discover()
+    int                 abort_pipe[2];
+};
 
 static netbios_ns_entry *netbios_ns_inverse_internal(netbios_ns *ns,
                                                      uint32_t ip);
@@ -168,6 +189,69 @@ static int netbios_ns_send_name_query(netbios_ns *ns,
 
     ns->last_trn_id = trn_id; // Remember the last transaction id.
     return 0;
+}
+
+static void netbios_ns_copy_name(char *dest, const char *src)
+{
+    memcpy(dest, src, NETBIOS_NAME_LENGTH);
+    dest[NETBIOS_NAME_LENGTH] = 0;
+
+    for (int i = 1; i < NETBIOS_NAME_LENGTH; i++ )
+      if (dest[NETBIOS_NAME_LENGTH - i] == ' ')
+        dest[NETBIOS_NAME_LENGTH - i] = 0;
+      else
+        break;
+}
+
+static netbios_ns_entry *netbios_ns_entry_add(netbios_ns *ns, const char *name,
+                                              const char *group,
+                                              char type, uint32_t ip)
+{
+    netbios_ns_entry  *entry;
+
+    entry = calloc(1, sizeof(netbios_ns_entry));
+    if (!entry)
+        return NULL;
+
+    if (name != NULL)
+        netbios_ns_copy_name(entry->name, name);
+    if (group != NULL)
+        netbios_ns_copy_name(entry->group, group);
+
+    entry->type           = type;
+    entry->address.s_addr = ip;
+    entry->next           = ns->entries;
+
+    ns->entries = entry;
+
+    return (ns->entries);
+}
+
+// Find an entry in the list. Search by name if name is not NULL,
+// or by ip otherwise
+static netbios_ns_entry *netbios_ns_entry_find(netbios_ns *ns, const char *by_name,
+                                               uint32_t ip)
+{
+    netbios_ns_entry  *found = NULL;
+    netbios_ns_entry  *iter;
+
+    assert(ns != NULL);
+
+    iter = ns->entries;
+    while (iter != NULL && found == NULL)
+    {
+        if (by_name != NULL)
+        {
+            if (!strncmp(by_name, iter->name, NETBIOS_NAME_LENGTH))
+                found = iter;
+        }
+        else if (iter->address.s_addr == ip)
+            found = iter;
+
+        iter = iter->next;
+    }
+
+    return (found);
 }
 
 netbios_ns  *netbios_ns_new()
@@ -428,4 +512,73 @@ const char *netbios_ns_inverse(netbios_ns *ns, uint32_t ip)
     assert(ns != NULL && ip != 0);
     netbios_ns_entry *entry = netbios_ns_inverse_internal(ns, ip);
     return entry ? entry->name : NULL;
+}
+
+const char *netbios_ns_entry_name(netbios_ns_entry *entry)
+{
+    return entry ? entry->name : NULL;
+}
+
+const char *netbios_ns_entry_group(netbios_ns_entry *entry)
+{
+    return entry ? entry->group : NULL;
+}
+
+uint32_t netbios_ns_entry_ip(netbios_ns_entry *entry)
+{
+    return entry ? entry->address.s_addr : 0;
+}
+
+char netbios_ns_entry_type(netbios_ns_entry *entry)
+{
+    return entry ? entry->type : -1;
+}
+
+void                netbios_ns_clear(netbios_ns *ns)
+{
+    netbios_ns_entry  *next;
+
+    assert(ns != NULL);
+
+    while (ns->entries != NULL)
+    {
+        next = ns->entries->next;
+        free(ns->entries);
+        ns->entries = next;
+    }
+}
+
+int             netbios_ns_entry_count(netbios_ns *ns)
+{
+    netbios_ns_entry  *iter;
+    int                 res;
+
+    assert(ns != NULL);
+
+    iter  = ns->entries;
+    res   = 0;
+    while (iter != NULL)
+    {
+        res++;
+        iter = iter->next;
+    }
+
+    return (res);
+}
+
+netbios_ns_entry  *netbios_ns_entry_at(netbios_ns *ns, int pos)
+{
+    netbios_ns_entry  *iter = NULL;
+    int                 i = 0;
+
+    assert(ns != NULL);
+
+    iter = ns->entries;
+    while (i < pos && iter != NULL)
+    {
+        i++;
+        iter = iter->next;
+    }
+
+    return (iter);
 }
