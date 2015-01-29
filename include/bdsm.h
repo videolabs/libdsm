63d983115606b992ace76306c5b7225c8844c8eb
Thomas Guillem <thomas@gllm.fr>
2015-01-29 18:02:23 +0100

    update license headers: LGPLv2.1 + commercial

--- include/bdsm.h
+++ include/bdsm.h
@@ -1,20 +1,32 @@
-//---------------------------------------------------------------------------
-//  __________________    _________  _____            _____  .__         ._.
-//  \______   \______ \  /   _____/ /     \          /  _  \ |__| ____   | |
-//   |    |  _/|    |  \ \_____  \ /  \ /  \        /  /_\  \|  _/ __ \  | |
-//   |    |   \|    `   \/        /    Y    \      /    |    |  \  ___/   \|
-//   |______  /_______  /_______  \____|__  / /\   \____|__  |__|\___ |   __
-//          \/        \/        \/        \/  )/           \/        \/   \/
-//
-// This file is part of libdsm. Copyright © 2014 VideoLabs SAS
-//
-// Author: Julien 'Lta' BALLET <contact@lta.io>
-//
-// This program is free software. It comes without any warranty, to the extent
-// permitted by applicable law. You can redistribute it and/or modify it under
-// the terms of the Do What The Fuck You Want To Public License, Version 2, as
-// published by Sam Hocevar. See the COPYING file for more details.
-//----------------------------------------------------------------------------
+/*****************************************************************************
+ *  __________________    _________  _____            _____  .__         ._.
+ *  \______   \______ \  /   _____/ /     \          /  _  \ |__| ____   | |
+ *   |    |  _/|    |  \ \_____  \ /  \ /  \        /  /_\  \|  _/ __ \  | |
+ *   |    |   \|    `   \/        /    Y    \      /    |    |  \  ___/   \|
+ *   |______  /_______  /_______  \____|__  / /\   \____|__  |__|\___ |   __
+ *          \/        \/        \/        \/  )/           \/        \/   \/
+ *
+ * This file is part of liBDSM. Copyright © 2014-2015 VideoLabs SAS
+ *
+ * Author: Julien 'Lta' BALLET <contact@lta.io>
+ *
+ * liBDSM is released under LGPLv2.1 (or later) and is also available
+ * under a commercial license.
+ *****************************************************************************
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
 
 #ifndef __BDSM_H_
 #define __BDSM_H_
