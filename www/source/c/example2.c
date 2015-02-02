3b86cc0ace8865bb66da0d3721899d8b7e0e0867
Thomas Guillem <thomas@gllm.fr>
2015-02-02 15:10:10 +0100

    www: reindent exemple

--- www/source/c/example2.c
+++ www/source/c/example2.c
@@ -20,12 +20,14 @@ static void print_entry(const char *what, void *p_opaque,
     netbios_ns_entry_type(entry));
 }
 
-static void on_entry_added(void *p_opaque, netbios_ns_entry *entry)
+static void on_entry_added(void *p_opaque,
+                           netbios_ns_entry *entry)
 {
   print_entry("added", p_opaque, entry);
 }
 
-static void on_entry_removed(void *p_opaque, netbios_ns_entry *entry)
+static void on_entry_removed(void *p_opaque,
+                             netbios_ns_entry *entry)
 {
   print_entry("removed", p_opaque, entry);
 }
@@ -43,7 +45,7 @@ int main()
 
   printf("Discovering...\nPress Enter to quit\n");
   if (!netbios_ns_discover_start(ns,
-                                 4, // broadcast every 4 seconds
+                                 4, // broadcast every 4 sec
                                  &callbacks))
   {
     fprintf(stderr, "Error while discovering local network\n");
