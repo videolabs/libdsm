7636c12c43d63c6594de09b1d944608c6be91089
Thomas Guillem <thomas@gllm.fr>
2015-05-20 17:45:00 +0200

    bin/dsm: fix double destroy

--- bin/dsm.c
+++ bin/dsm.c
@@ -243,7 +243,6 @@ int main(int ac, char **av)
   if (st != NULL)
   {
     printf("File '%s' is %"PRIu64" bytes long\n", fname, smb_stat_get(st, SMB_STAT_SIZE));
-    smb_stat_destroy(files);
   }
 
 
