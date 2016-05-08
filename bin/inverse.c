9ecf9ef68ef17e71509af913d47091eb829ceffd
Jean-Baptiste Kempf <jb@videolan.org>
2016-05-09 00:26:52 +0200

    inverse: fast replacement for Windows missing inet_ntop

--- bin/inverse.c
+++ bin/inverse.c
@@ -41,6 +41,22 @@
 
 #include "bdsm.h"
 
+#ifdef _WIN32
+#define EAFNOSUPPORT WSAEAFNOSUPPORT
+int inet_pton (int af, const char *src, void *dst)
+{
+    unsigned char *b = dst;
+    switch (af)
+    {
+        case AF_INET:
+            return sscanf (src, "%hhu.%hhu.%hhu.%hhu",
+                    b + 0, b + 1, b + 2, b + 3) == 4;
+    }
+    errno = EAFNOSUPPORT;
+    return -1;
+}
+#endif
+
 int main(int ac, char **av)
 {
   netbios_ns        *ns;
