44a3e3ecbc49794bc20632ddb7622687670cfec2
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-05-10 17:33:51 +0200

    Add missing config.h
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/hmac_md5.c
+++ src/hmac_md5.c
@@ -28,6 +28,8 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#include "config.h"
+
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
