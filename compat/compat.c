1e318e49decf2c6d882f81ca28e3c013ad3c6cb4
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-05-27 13:57:22 +0200

    compat.c is not empty anymore

--- compat/compat.c
+++ compat/compat.c
@@ -1,14 +1,5 @@
 #include "config.h"
 
-/* This file is just a placeholder so the library isn't empty */
-
-/* This function does strictly nothing. It's just here to avoid
-   libcompat.a to be empty, which is illegal */
-int not_empty()
-{
-    return 42;
-}
-
 #if !defined(HAVE_PIPE) && defined(HAVE__PIPE)
 
 #include <fcntl.h>
