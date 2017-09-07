bdf7de7a0618c620736091e74afc0a978299261d
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-09-07 10:49:38 +0200

    Bump ABI revision

--- include/bdsm.h
+++ include/bdsm.h
@@ -31,9 +31,9 @@
 #ifndef __BDSM_H_
 #define __BDSM_H_
 
-#define BDSM_VERSION_CURRENT  3
-#define BDSM_VERSION_REVISION 1
-#define BDSM_VERSION_AGE      0
+#define BDSM_VERSION_CURRENT  4
+#define BDSM_VERSION_REVISION 0
+#define BDSM_VERSION_AGE      1
 
 #include "bdsm/netbios_ns.h"
 #include "bdsm/netbios_defs.h"
